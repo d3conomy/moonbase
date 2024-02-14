2a07167d556ebcfa51622eb64f5fd4e542facb00
boyroywax <jmsdevln@gmail.com>
2024-02-13 16:17:06 -0800

    moonbase:setup:configuring testing

--- dist/src/utils/index.js
+++
@@ -1,9 +0,0 @@
-import { logger, getLogBook, LogBook, LogBooksManager } from './logBook.js';
-const createRandomId = () => {
-    return Math.random().toString(36).substring(2, 15);
-};
-export { 
-// Misc. Tools
-createRandomId, 
-// Log Book
-logger, getLogBook, LogBook, LogBooksManager };
