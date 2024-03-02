9de82e7510dafa083a0e22e21a7b64c48c245203
boyroywax <jmsdevln@gmail.com>
2024-03-02 11:03:25 -0800

    orbitdb: more tests added for orbitdb cration

--- src/tests/setupOrbitDb.spec.ts
+++ src/tests/setupOrbitDb.spec.ts
@@ -3,6 +3,8 @@ import { _IpfsOptions, IpfsProcess } from '../db/ipfs.js';
 import { Libp2pProcess } from '../db/libp2p.js';
 
 import { expect } from 'chai';
+import { logger } from '../utils/logBook.js';
+import { LogLevel } from '../utils/constants.js';
 
 describe('CreateOrbitDbProcess', async () => {
     let libp2p: Libp2pProcess;
@@ -37,6 +39,50 @@ describe('CreateOrbitDbProcess', async () => {
         // Add more assertions based on your specific requirements
 
         // Clean up
+        await orbitDb.stop();
+        await ipfs.stop();
+        await libp2p.stop();
+    });
+
+    it('should fail creating simultaneous OrbitDb instances from same ipfs', async () => {
+        // Create a mock libp2p instance
+        libp2p = new Libp2pProcess({});
+        await libp2p.init();
+
+        // Create the options object
+        const options: _IpfsOptions = new _IpfsOptions({
+            libp2p,
+        });
+
+        // Call the createIpfsProcess function
+        ipfs = new IpfsProcess({ options });
+        await ipfs.init();
+
+        // Create the OrbitDb Options
+        const orbitDbOptions = new _OrbitDbOptions({
+            ipfs,
+        });
+
+        // Call the createOrbitDbProcess function
+        orbitDb = new OrbitDbProcess({ options: orbitDbOptions});
+        await orbitDb.init();
+
+        logger({
+            level: LogLevel.INFO,
+            message: `OrbitDb id: ${orbitDb.id.getId()}`
+        })
+
+        // Create a second OrbitDb instance
+        try {
+            const orbitDb2 = new OrbitDbProcess({ options: orbitDbOptions});
+            await orbitDb2.init();
+        }
+        catch (error) {
+            // Assert that the second OrbitDb instance creation fails
+            expect(error).to.be.an('error');
+            // Add more assertions based on your specific requirements
+        }
+
         await orbitDb.stop();
         await ipfs.stop();
         await libp2p.stop();
